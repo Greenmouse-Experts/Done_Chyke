76b640439ec5b0dffe72414a1cdb42404ad0f1d0
Promise <promiseezema11@gmail.com>
2023-06-11 11:42:57 +0100

    updated

--- resources/views/accountant/daily_balance.blade.php
+++ resources/views/accountant/daily_balance.blade.php
@@ -70,7 +70,7 @@
                                  <th class="text-center prc-wrap">
                                     <div class="prc-box active">
                                        <div class="h3 pt-4 text-white">₦{{number_format($starting_balance, 2)}}
-                                       </div> <span class="type text-white">Starting Balance</span>
+                                       </div> <span class="type text-white">Starting <br> Balance</span>
                                     </div>
                                  </th>
                                  <th class="text-center prc-wrap">
