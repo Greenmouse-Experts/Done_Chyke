509f6354107cb0a05097dcec1d089249111f72ea
Promise <promiseezema11@gmail.com>
2023-09-25 15:26:22 +0100

    updated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -71,7 +71,6 @@ class DashboardController extends Controller
 
         $expensesCash = Expenses::where('payment_source', 'Cash')->whereDate('date', $today)->get()->sum('amount');
 
-
         $expenses = Expenses::whereDate('date', $yesterday)->get()->sum('amount');
 
         $paymentsDateCash = Payment::where('payment_type', 'Cash')->whereDate('date_paid', $yesterday)->get();
@@ -88,6 +87,10 @@ class DashboardController extends Controller
 
         $closing_balance = $expenses + $cash + $transfer + $cheques;
 
+        $expensesCash = Expenses::where('payment_source', 'Cash')->whereDate('date', $yesterday)->get()->sum('amount');
+        $expensesCheque = Expenses::where('payment_source', 'Transfer by Cheques')->whereDate('date', $yesterday)->get()->sum('amount');
+        $expensesTransfer = Expenses::where('payment_source', 'Direct Transfer')->whereDate('date', $yesterday)->get()->sum('amount');
+
         return view('dashboard.dashboard', [
             'moment' => $moment,
             'notifications' => $notifications,
@@ -96,8 +99,9 @@ class DashboardController extends Controller
             'totalStartingBalance' => $totalStartingBalance,
             'paymentCash' => $paymentCash,
             'expensesCash' => $expensesCash,
-            'closing_balance' => $closing_balance
-
+            'closing_balance' => $closing_balance,
+            'direct_transfer' => $expensesTransfer + $transfer,
+            'transfer_cheque' => $expensesCheque + $cheques
         ]);
     }
 
